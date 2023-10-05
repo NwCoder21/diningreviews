6365b37a439567b82c34e88a8deb2e366f21066e
NwCoder21 <usmanfarooq_2021@hotmail.com>
2023-10-05 12:22:38 +0100

    added @Table(name = "dining_Reviews") to model/DiningReview.java

--- diningreviews/src/main/java/com/myproject/diningreviews/model/DiningReview.java
+++ diningreviews/src/main/java/com/myproject/diningreviews/model/DiningReview.java
@@ -1,10 +1,7 @@
 package com.myproject.diningreviews.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -19,7 +16,7 @@ import lombok.Setter;
 public class DiningReview {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String submittedBy;
